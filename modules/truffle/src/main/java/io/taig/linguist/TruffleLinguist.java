3af5db1476b0d8e43f93ca1550868e15c0fa7223
Niklas Klein <mail@taig.io>
2021-07-29 12:12:23 +0200

    Use Polyglot.import to access Java arguments

--- modules/truffle/src/main/java/io/taig/linguist/TruffleLinguist.java
+++ modules/truffle/src/main/java/io/taig/linguist/TruffleLinguist.java
@@ -1,6 +1,7 @@
 package io.taig.linguist;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.PolyglotAccess;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
@@ -12,20 +13,19 @@ public class TruffleLinguist implements Linguist, AutoCloseable  {
   private final Context context;
 
   public TruffleLinguist() {
-    this.context = Context.newBuilder("ruby").allowNativeAccess(true).allowIO(true).build();
+    this.context = Context.newBuilder("ruby").allowNativeAccess(true).allowIO(true).allowPolyglotAccess(PolyglotAccess.ALL).build();
   }
 
   @Override
   public Optional<String> detect(Path path, String content) {
-    Source source;
-    try {
-      source = Source.newBuilder("ruby",
+    final Value bindings = context.getPolyglotBindings();
+    bindings.putMember("path", path.toString());
+    bindings.putMember("content", content);
+
+    final Source source = Source.create("ruby",
         "require 'linguist'\n" +
-          "blob = Linguist::Blob.new('" + path + "', '" + content + "')\n" +
-          "Linguist.detect(blob)&.name", "detection.rb").build();
-    } catch (IOException exception) {
-      throw new IllegalStateException("Failed to construct ruby Source");
-    }
+          "blob = Linguist::Blob.new(Polyglot.import('path'), Polyglot.import('content'))\n" +
+          "Linguist.detect(blob)&.name");
 
     final Value result = context.eval(source);
 
