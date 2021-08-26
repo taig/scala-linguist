7b5f70e90d4c9ff8e658ba42d8146142cec869cf
Niklas Klein <mail@taig.io>
2021-08-26 23:30:32 +0200

    [#2] Pooled instance

--- modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
+++ modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
@@ -2,6 +2,10 @@ package io.taig.linguist
 
 import cats.effect.{IO, Resource}
 
-final class GraalVmRubyLinguistIntegrationTest extends LinguistTest {
+final class DefaultGraalVmRubyLinguistIntegrationTest extends LinguistTest {
   override val linguist: Resource[IO, Linguist[IO]] = GraalVmRubyLinguist.default[IO]
+}
+
+final class PooledGraalVmRubyLinguistIntegrationTest extends LinguistTest {
+  override val linguist: Resource[IO, Linguist[IO]] = GraalVmRubyLinguist.pooled[IO](size = 2)
 }
