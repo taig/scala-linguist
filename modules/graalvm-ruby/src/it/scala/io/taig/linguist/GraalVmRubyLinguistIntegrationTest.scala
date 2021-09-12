8436d109ee5e5a65c2678a38bde960d64319f9be
Niklas Klein <mail@taig.io>
2021-09-12 14:17:57 +0200

    Try increasing test timeouts for CI

--- modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
+++ modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
@@ -2,10 +2,16 @@ package io.taig.linguist
 
 import cats.effect.{IO, Resource}
 
+import scala.concurrent.duration._
+
 final class DefaultGraalVmRubyLinguistIntegrationTest extends LinguistTest {
+  override val munitTimeout: Duration = 60.seconds
+
   override val linguist: Resource[IO, Linguist[IO]] = GraalVmRubyLinguist.default[IO]
 }
 
 final class PooledGraalVmRubyLinguistIntegrationTest extends LinguistTest {
+  override val munitTimeout: Duration = 60.seconds
+
   override val linguist: Resource[IO, Linguist[IO]] = GraalVmRubyLinguist.pooled[IO](size = 3)
 }
