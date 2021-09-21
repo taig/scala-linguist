e596e3526e5ea2f4854c01b9bbcf419724e83e4b
Niklas Klein <mail@taig.io>
2021-09-22 00:01:53 +0200

    Need higher test timeout for CI

--- modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
+++ modules/graalvm-ruby/src/it/scala/io/taig/linguist/GraalVmRubyLinguistIntegrationTest.scala
@@ -2,7 +2,11 @@ package io.taig.linguist
 
 import cats.effect.{IO, Resource}
 
+import scala.concurrent.duration._
+
 abstract class GraalVmRubyLinguistIntegrationTest extends LinguistTest {
+  override val munitTimeout: Duration = 60.seconds
+
   def resource: Resource[IO, Linguist[IO]]
 
   override val linguist: Fixture[Linguist[IO]] = new Fixture[Linguist[IO]]("linguist") {
